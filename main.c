5d2a564dc8774e53473f5f4e04b02e3526a8a31c
Quintin <quintinp311204@gmail.com>
2024-10-08 11:48:43 -0500

    fahr to cels

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+/* print fahrenheight-celsius table
+    for f = 0, 20, ..., 300 */
 int main() { 
-    printf("Hello, Wor!\n");
+    int lower, upper, step;
+    float fahr, celsius;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%4.0f %6.0f\n", fahr, celsius);
+        fahr += step;
+    }
     return 0;
 }
