7f54331b9bbe8d547627f26eda9620f08ce7e3cc
vuintin <quintinp311204@gmail.com>
2024-10-08 22:08:25 -0500

    2nd edition

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
     for f = 0, 20, ..., 300 */
 int main() { 
     int lower, upper, step; // declaring variables with type int (integers)
-    float fahr, celsius; // declaring variables with type float (floating point)
+    int fahr, celsius; // declaring variables with type float (floating point)
 
     /* More basic data types: 
         char: character - a single byte
@@ -14,16 +14,15 @@ int main() {
 
 
 
-    lower = 0; // setting the variables to their starting values
-    upper = 300;
-    step = 20;
+    lower = 0; // setting the variables to their starting values (lower limit)
+    upper = 300; // upper limit
+    step = 20; // step size
 
     fahr = lower;
     printf("Fahr-Celsius Conversion Table\n");
     while (fahr <= upper) { // while loop. condition in parantheses is tested and code is ran while true
-        celsius = (5.0/9.0) * (fahr-32.0);
-        
-        printf("%4.0f %6.1f\n", fahr, celsius); /* %4.0 says float number is to be printed in a space atleast 4 characters wide 
+        celsius = 5 * (fahr-32.0) / 9;
+        printf("%d\t%d\n", fahr, celsius); /* %4.0 says float number is to be printed in a space atleast 4 characters wide 
                             with no digits after the decimal point. %6.1f is 6 spaces and 1 decimal point */ 
         fahr += step;
     }
